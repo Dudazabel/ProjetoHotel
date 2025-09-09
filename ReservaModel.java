109d9be41f0229af0411bedc53557af264467d53
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2025-09-09 14:23:52 -0300

    MZ feat: implementa nova classe

--- ReservaModel.java
+++ ReservaModel.java
@@ -2,12 +2,35 @@ public class ReservaModel {
 
     private String dataEntrada;
     private String dataSaida;
-
-    QuartoModel quartoModel = new QuartoModel();
-    HospedeModel hospedeModel = new HospedeModel();
+    private QuartoModel quartoModel = new QuartoModel();
+    private HospedeModel hospedeModel = new HospedeModel();
 
     public ReservaModel(String nome, int numero, String dataEntrada, String dataSaida){
-        
+        this.hospedeModel.setNome(nome);
+        this.quartoModel.setNumero(numero);
+        this.dataEntrada= dataEntrada;
+        this.dataSaida = dataSaida;
+    }
+
+    public void setDataEntrada(String dataEntrada){
+        this.dataEntrada = dataEntrada;
+    }
+
+    public void setDataSaida(String dataSaida){
+        this.dataSaida = dataSaida;
+    }
+
+    public String getDataEntrada(){
+        return this.dataEntrada;
+    }
+
+    public String getDataSaida(){
+        return this.dataSaida;
+    }
+
+    @Override
+    public String toString(){
+        return "Nome: " + hospedeModel.getNome() + " | Número do Quarto: " + quartoModel.getNumero() + " | Data de Entrada: " + dataEntrada + " | Data de Saída: " + dataSaida;
     }
     
 }
